5cdcc3f09aa1e974df3797436a7b69b74292b98c
Massimo Melina <a@rejetto.com>
2024-04-23 09:57:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# 404
+HFS plugin to redirect users on not-found
+
+HFS ~ HTTP File Server https://github.com/rejetto/hfs
---
+++ dist/plugin.js
@@ -0,0 +1,22 @@
+exports.version = 1
+exports.apiRequired = 1
+exports.description = "Redirect users on not-found"
+exports.repo = 'rejetto/404'
+
+exports.config = {
+    url: { label:"URL", helperText: "Where to redirect" }
+}
+
+exports.init = api => ({
+    middleware(ctx) {
+        if (!ctx.path.startsWith(api.Const.SPECIAL_URI)  // special uris should be excluded...
+            && ctx.path !== '/favicon.ico')
+            return () => { // execute after other middlewares are done
+                if (ctx.status !== 404) return
+                const url = api.getConfig('url')
+                if (!url) return
+                ctx.redirect(url)
+                return true
+            }
+    }
+})
